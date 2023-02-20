66dabcd5658f687829677f45c1e808dd0ae62e37
Italo Ferreira <grd788@gmail.com>
2023-02-19 23:08:04 -0500

    feat :sparkles: page history table done

--- projeto-02-ignitetimer/src/pages/History/index.tsx
+++ projeto-02-ignitetimer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,31 +20,41 @@ export function History() {
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há dois meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há dois meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há dois meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há dois meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há dois meses</td>
-              <td>Em andamento</td>
+              <td>
+                <Status statusColor="green">Em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
