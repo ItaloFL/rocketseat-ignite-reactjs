4d55ea1f2c3cfe7b833fd0c7bb48dc22922f5842
Italo Ferreira <grd788@gmail.com>
2023-02-23 23:34:08 -0500

    feat :spkarles: minutes and seconds cycle

--- projeto-02-ignitetimer/src/pages/Home/index.tsx
+++ projeto-02-ignitetimer/src/pages/Home/index.tsx
@@ -9,13 +9,60 @@ import {
   TaskInput
 } from './styles'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useState } from 'react'
+
+const newCountFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser no maximo 60 minutos')
+})
+
+type newCycleFormData = zod.infer<typeof newCountFormValidationSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
-  function handleCreateNewCount(data: any) {
-    console.log(data)
+  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
+    resolver: zodResolver(newCountFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  function handleCreateNewCount(data: newCycleFormData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount
+    }
+
+    setCycles(state => [...state, newCycle])
+    setActiveCycleId(newCycle.id)
+    reset()
   }
+
+  const activeCyle = cycles.find(cycle => cycle.id === activeCycleId)
+  const totalSeconds = activeCyle ? activeCyle.minutesAmount * 60 : 0
+  const currentSeconds = activeCyle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
 
   const task = watch('task')
   const isSubmitDisabled = !task
@@ -44,18 +91,16 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
-            min={5}
-            max={60}
-            {...register('minutesAmount')}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountDownContainer>
         <StartCoutdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
